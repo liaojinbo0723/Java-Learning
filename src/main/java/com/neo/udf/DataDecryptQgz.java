28b40121266eddc04fc72426cbac26af88a6a568
xn043275 <liaojinbo@xiaoniu66.com>
2018-01-29 15:40:39 +0800

    update

--- src/main/java/com/neo/udf/DataDecryptQgz.java
+++ src/main/java/com/neo/udf/DataDecryptQgz.java
@@ -5,13 +5,18 @@ import com.xiaoniu.encript.EncriptUtils;
 
 public class DataDecryptQgz extends UDF{
 
-    private String getData(String strEncrypt){
+    /**
+     * 重写evaluate方法
+     * @param strEncrypt
+     * @return
+     */
+    private String evaluate(String strEncrypt){
         String strData = EncriptUtils.decript(strEncrypt);
         return strData;
     }
 
     public static void main(String[] args) {
         DataDecryptQgz ddq = new DataDecryptQgz();
-        System.out.println(ddq.getData("TOqJ+Ks24/vY12j6PbW68HxCOFowpn711haHrG9d9fc="));
+        System.out.println(ddq.evaluate("TOqJ+Ks24/vY12j6PbW68HxCOFowpn711haHrG9d9fc="));
     }
 }
