bff15b2f8d23ebd9e11fb4a0c6cdcb457ce2b803
xn043275 <liaojinbo@xiaoniu66.com>
2018-02-01 22:05:04 +0800

    update

--- src/main/java/com/neo/datamanager/ForConnMysql.java
+++ src/main/java/com/neo/datamanager/ForConnMysql.java
@@ -56,22 +56,26 @@ public class ForConnMysql {
 		String dbname ;
 		String colname ;
 		ForConnMysql fc = new ForConnMysql();
-		Connection conn = fc.connMysql("jdbc:mysql://10.17.2.134:3306/data_dev","root","xiaoniu1234");
-		String sql = "select t.src_table_name,t.src_column_name from data_dev.src_table_detail t limit 10; ";
-		ResultSet rs = fc.querySql(sql,conn);
-		try {
-			while (rs.next()) {
-				dbname = rs.getString(1);
-				colname = rs.getString(2);
-				System.out.println("db:" + dbname + "\t" + "col_name:" + colname);
-			}
-			rs.close();//关闭游标
-			conn.close();//关闭连接
-		}
-		catch (SQLException e){
-			System.out.println("conn is failed!!");
-		}
-		System.out.println("hello");
+		Connection conn = fc.connMysql("jdbc:mysql://10.8.34.67:3306/dm","zxbi_rw","y-1stpp6RkpY");
+		String sql = "load data local infile 'result.txt' into table adw.t_user_person_temp fields terminated by '\t'";
+		fc.dmlSql(sql,conn);
+
+//		String sql = "select t.src_table_name,t.src_column_name from data_dev.src_table_detail t limit 10; ";
+
+//		ResultSet rs = fc.querySql(sql,conn);
+//		try {
+//			while (rs.next()) {
+//				dbname = rs.getString(1);
+//				colname = rs.getString(2);
+//				System.out.println("db:" + dbname + "\t" + "col_name:" + colname);
+//			}
+//			rs.close();//关闭游标
+//			conn.close();//关闭连接
+//		}
+//		catch (SQLException e){
+//			System.out.println("conn is failed!!");
+//		}
+//		System.out.println("hello");
 	}
 }
 
--- src/main/java/com/neo/datamanager/Test.java
+++ src/main/java/com/neo/datamanager/Test.java
@@ -5,9 +5,15 @@ public class Test  {
 
     public static void main(String[] args) {
         String strJobId = "123123";
+        System.out.println("strJobId = " + strJobId);
         String strUpdateSql = "update mss.cm_run_job set state = 3 where job_id = '"
                             + strJobId
                             + "' and state = 2";
         System.out.println(strUpdateSql);
+        System.out.println("strUpdateSql = " + strUpdateSql);
+        System.out.println("123".concat("aa"));
+        
+        String startDate = args[0];
+        System.out.println("startDate = " + startDate);
     }
 }
