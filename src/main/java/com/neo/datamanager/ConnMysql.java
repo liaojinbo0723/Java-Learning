fe0bb4f7b16410a717a52ba36e1e60c5785aa428
xn043275 <liaojinbo@xiaoniu66.com>
2018-02-24 17:31:56 +0800

    update

--- src/main/java/com/neo/datamanager/ConnMysql.java
+++ src/main/java/com/neo/datamanager/ConnMysql.java
@@ -1,64 +1,62 @@
 package com.neo.datamanager;
 
 import java.sql.*;
+
 /**
  * java 连接mysql公共类
  */
 
 public class ConnMysql {
-	public static final String driverName = "com.mysql.jdbc.Driver";
-	public Connection connMysql(String url,String user,String pass){
-		Connection conn = null;
-		try {
-			Class.forName(driverName);//加载驱动
-			conn = DriverManager.getConnection(url,user,pass);//建立连接
-		}
-		catch (ClassNotFoundException e)
-		{
-			System.out.println("Sorry,can`t find the Driver!");
-			e.printStackTrace();
-		}
-		catch (SQLException e){
-			e.printStackTrace();
-		}
-		return conn;
-	}
 
-	public ResultSet querySql(String v_sql,Connection conn){
-		ResultSet rs = null;
-		try{
-			Statement stm = conn.createStatement();//获取句柄，用来执行sql
-			rs = stm.executeQuery(v_sql);//执行sql
-		}
-		catch (SQLException e){
-			e.printStackTrace();
-		}
-		return rs;
-	}
+    public static final String driverName = "com.mysql.jdbc.Driver";
 
-	public boolean dmlSql(String v_sql,Connection conn){
-		boolean flag;
-		try{
-			Statement stm = conn.createStatement();//获取句柄，用来执行sql
-			stm.execute(v_sql);
-			flag = true;
-		}
-		catch (SQLException e){
-			flag = false;
-			System.out.println("sql except");
-			e.printStackTrace();
-		}
-		return flag;
-	}
+    public Connection connMysql(String url, String user, String pass) {
+        Connection conn = null;
+        try {
+            Class.forName(driverName);//加载驱动
+            conn = DriverManager.getConnection(url, user, pass);//建立连接
+        } catch (ClassNotFoundException e) {
+            System.out.println("Sorry,can`t find the Driver!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 
-	public static void main(String[] args) {
-		System.out.println("fuck you!!");
-		String dbname ;
-		String colname ;
-		ConnMysql fc = new ConnMysql();
-		Connection conn = fc.connMysql("jdbc:mysql://10.8.34.67:3306/dm","zxbi_rw","y-1stpp6RkpY");
-		String sql = "load data local infile 'result.txt' into table adw.t_user_person_temp fields terminated by '\t'";
-		fc.dmlSql(sql,conn);
+    public ResultSet querySql(String v_sql, Connection conn) {
+        ResultSet rs = null;
+        try {
+            Statement stm = conn.createStatement();//获取句柄，用来执行sql
+            rs = stm.executeQuery(v_sql);//执行sql
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public boolean dmlSql(String v_sql, Connection conn) {
+        boolean flag;
+        try {
+            Statement stm = conn.createStatement();//获取句柄，用来执行sql
+            stm.execute(v_sql);
+            flag = true;
+        } catch (SQLException e) {
+            flag = false;
+            System.out.println("sql except");
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("fuck you!!");
+        String dbname;
+        String colname;
+        ConnMysql fc = new ConnMysql();
+        Connection conn = fc.connMysql("jdbc:mysql://10.8.34.67:3306/dm", "zxbi_rw", "y-1stpp6RkpY");
+        String sql = "load data local infile 'result.txt' into table adw.t_user_person_temp fields terminated by '\t'";
+        fc.dmlSql(sql, conn);
 
 //		String sql = "select t.src_table_name,t.src_column_name from data_dev.src_table_detail t limit 10; ";
 
@@ -76,6 +74,6 @@ public class ConnMysql {
 //			System.out.println("conn is failed!!");
 //		}
 //		System.out.println("hello");
-	}
+    }
 }
 
--- src/main/java/com/neo/datamanager/Test.java
+++ src/main/java/com/neo/datamanager/Test.java
@@ -1,19 +1,52 @@
 package com.neo.datamanager;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Test  {
 
+
+    /**
+     * 读取jar包内的配置文件
+     * @return
+     */
+    private static Properties getConfigInfoIn() {
+        InputStream is = Test.class.getResourceAsStream("/config2.properties");
+        Properties prop = new Properties();
+        try {
+            prop.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    /**
+     * 读取jar包外的配置文件
+     * @return
+     */
+    private static Properties getConfigInfoOut() {
+        String filePath = System.getProperty("user.dir")  + "\\config3.properties";
+        System.out.println(filePath);
+        InputStream is = null;
+        Properties prop = new Properties();
+        try {
+            is = new FileInputStream(filePath);
+            prop.load(is);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
     public static void main(String[] args) {
-        String strJobId = "123123";
-        System.out.println("strJobId = " + strJobId);
-        String strUpdateSql = "update mss.cm_run_job set state = 3 where job_id = '"
-                            + strJobId
-                            + "' and state = 2";
-        System.out.println(strUpdateSql);
-        System.out.println("strUpdateSql = " + strUpdateSql);
-        System.out.println("123".concat("aa"));
-        
-        String startDate = args[0];
-        System.out.println("startDate = " + startDate);
+        Properties prop = getConfigInfoOut();
+        System.out.println(prop.getProperty("user"));
     }
 }
