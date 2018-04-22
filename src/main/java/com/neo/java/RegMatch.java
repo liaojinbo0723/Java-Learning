6c7feb5a8c5055fa44d7142c5024d211e8502b80
liaojinbo <112253662@qq.com>
2018-04-23 00:11:28 +0800

    match

--- src/main/java/com/neo/java/RegMatch.java
+++ src/main/java/com/neo/java/RegMatch.java
@@ -8,18 +8,18 @@ import java.util.regex.Pattern;
 
 public class RegMatch {
 
-    public static List<String> getFile(String filePath){
+    public static List<String> getFile(String filePath) {
         File file = new File(filePath);
         List<String> list = new ArrayList();
         File[] files = file.listFiles();
         for (int i = 0; i < files.length; i++) {
             File f = files[i];
-            if (f.isFile()){
-                if (f.getName().contains(".kjb")){
+            if (f.isFile()) {
+                if (f.getName().contains(".kjb")) {
                     list.add(f.getAbsolutePath());
                 }
             }
-            if(f.isDirectory()){
+            if (f.isDirectory()) {
                 getFile(f.getAbsolutePath());
             }
         }
@@ -32,25 +32,26 @@ public class RegMatch {
         BufferedReader bReader = new BufferedReader(reader);
         StringBuilder sb = new StringBuilder();
         String s = "";
-        while ((s =bReader.readLine()) != null) {
+        while ((s = bReader.readLine()) != null) {
             sb.append(s + "\n");
         }
         bReader.close();
         return sb.toString().toLowerCase();
 
     }
+
     public static void matchJob(String file) throws IOException {
         List<String> list = new ArrayList();
         list.add("\\s+from\\s+(\\w+)[.](\\w+)");
         list.add("\\s+join\\s+(\\w+)[.](\\w+)");
         String sql = getString(file);
-        Pattern p=null;
+        Pattern p = null;
         String result = "";
         for (int i = 0; i < list.size(); i++) {
             p = Pattern.compile(list.get(i));
-            Matcher matcher=p.matcher(sql);
+            Matcher matcher = p.matcher(sql);
             while (matcher.find()) {
-                String string1 ="job_" + matcher.group(2);
+                String string1 = "job_" + matcher.group(2);
                 result = result + "@" + string1;
             }
         }
